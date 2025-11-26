6b1c5e837dd1b27dbf7a36aba9c9fb6627d166ba
KagisoMokgobi0123 <Kgmokgobi@gmail.com>
2025-11-26 18:51:26 +0200

    Edited the employeeController. Changed the bycryptjs to bycrypt.

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -1,6 +1,6 @@
 import Employee from "../models/Employee.js";
 import Role from "../models/Role.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcryptj";
 
 // GET ALL EMPLOYEES
 export const getEmployees = async (req, res) => {
